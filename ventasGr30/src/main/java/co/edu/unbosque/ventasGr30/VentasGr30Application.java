874381dbf1f54345250233d9d757e8f1d8af2f5e
Jorge <joradiazf@gmail.com>
2021-11-22 09:44:45 -0500

    pom dependencias

---
+++ ventasGr30/src/main/java/co/edu/unbosque/ventasGr30/VentasGr30Application.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ventasGr30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasGr30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasGr30Application.class, args);
+	}
+
+}
